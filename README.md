0acac2eac31764b6e555115e58bb5f69b097b6db
SPGoding <SPGoding@outlook.com>
2019-09-13 23:28:32 +0800

    :rewind: Revert three commits

--- README.md
+++ README.md
@@ -1,31 +1,9 @@
 # minecraft.net-translations
-## 说明
-
 目前只维护Culture和Insider分类。运行mcContents.py即可在culture.csv和insider.csv追加新发布的博文。
 
 MCBBS的官方博文录目前的收录更新到2018年6月3日《每周方块：音符盒》及其之前的帖子，这些帖子暂时不再同步到这里，请到论坛帖子中搜索。（但是也可能出现老文章后翻译的情况）
 
 如果API坏了，请联系https://github.com/SPGoding
-
-## 使用方法
-
-### 更新数据
-
-在目录下执行：
-
-```bash
-python mcContents.py [gb18030|utf-8]
-```
-
-不填写sync时，从官网更新数据到两个表中。
-
-填写sync时，将两个表中的内容同步为参数中指定编码表的内容。
-
-如，`python mcContents.py utf-8`将gb表同步为utf8表的内容。
-
-### 添加内容
-
-gb表支持使用excel编辑，utf8表支持使用主流文本编辑器（vscode, notepad++）编辑。
 
 ## 实用链接
 在线博文转换器：https://spgoding.com
--- mcContents.py
+++ mcContents.py
@@ -2,17 +2,13 @@ import pandas as pd
 from urllib import request
 import json
 import calendar
-import sys
-
-CATES = ['insider', 'culture']
-ENCODING = ['utf-8','gb18030']
 
 def update(cate: str):
     # 根据分类确定api
     apiurl = 'https://www.minecraft.net/content/minecraft-net/_jcr_content.articles.grid?tagsPath=minecraft:article/' + cate + '&lang=/content/minecraft-net/language-masters/zh-hans&pageSize=100'
 
     # 获取之前的记录
-    prev_data = pd.read_csv(cate+"_gb.csv", encoding='gb18030')
+    prev_data = pd.read_csv(cate+".csv", encoding='utf-8')
     last_title = prev_data.loc[0]["Article Title"]
 
     # 获取新的数据，建立空表
@@ -34,22 +30,8 @@ def update(cate: str):
 
     # 合并新旧表，保存
     pd.concat([new_article_data,prev_data]).to_csv(path_or_buf=cate+".csv", index=False, encoding='utf-8')
-    pd.concat([new_article_data,prev_data]).to_csv(path_or_buf=cate+"_gb.csv", index=False, encoding='gb18030')
     print("Saving", cate + ".")
 
 # 更新csv
-if sys.argv[-1] in ENCODING:
-    enc = sys.argv[-1]
-    for cate in CATES:
-        if enc == 'utf-8':
-            print("Set "+cate+"_gb.csv to "+cate+".csv")
-            prev_data = pd.read_csv(cate+".csv", encoding='utf-8')
-            prev_data.to_csv(path_or_buf=cate+"_gb.csv", index=False, encoding='gb18030')
-        else:
-            print("Set "+cate+".csv to "+cate+"_gb.csv")
-            prev_data = pd.read_csv(cate+"_gb.csv", encoding='gb18030')
-            prev_data.to_csv(path_or_buf=cate+".csv", index=False, encoding='utf-8')
-else:
-    for cate in CATES:
-        update(cate)
-
+update('insider')
+update('culture')
