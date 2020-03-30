f4320a1bc53d36542290fff9b31f9ff93e21c213
SPGoding <SPGoding@outlook.com>
2020-03-30 00:25:26 -0500

    :bug: fix sync

--- sync.js
+++ sync.js
@@ -62,43 +62,53 @@ async function saveRawTable() {
  * @param {string} link
  */
 async function getAuthorUid(un, link) {
-    if (username[un]) {
-        return username[un]
+    try {
+        if (username[un]) {
+            return username[un]
+        }
+        const regex = /<div class="authi"><a href="home\.php\?mod=space&amp;uid=(\d+?)"/
+        const html = await get(link)
+        const [, uid] = regex.exec(html)
+        username[un] = uid
+        return uid
     }
-    const regex = /<div class="authi"><a href="home\.php\?mod=space&amp;uid=(\d+?)"/
-    const html = await get(link)
-    const [, uid] = regex.exec(html)
-    username[un] = uid
-    return uid
+    catch (e) {
+        console.error(e)
+        return ''
+    }
 }
 
 async function merge() {
-    const regex = /<a href="https?:\/\/(?:www\.)?minecraft\.net\/.+?\/article\/(.+?)" target="_blank">.+?<\/a><\/td><td width="30%"><a href="(.+?)" target="_blank">(.+?)<\/a><\/td><td width="25%">(.+?)<\/td><\/tr>/g
+    try {
+        const regex = /<a href="https?:\/\/(?:www\.)?minecraft\.net\/.+?\/article\/(.+?)" target="_blank">.+?<\/a><\/td><td width="30%"><a href="(.+?)" target="_blank">(.+?)<\/a><\/td><td width="25%">(.+?)<\/td><\/tr>/g
 
-    const html = await get(McbbsIndexUri)
-    let match = regex.exec(html)
-    while (match) {
-        const [, link, trLink, trTitle, trUsername] = match
-        if (/^https?:\/\/(?:www\.)?mcbbs\.net\/thread\-\d+\-\d+\-\d+\.html?$/.test(trLink) && trUsername !== '-') {
-            const row = rawTable.find(r => r[2] && r[2].endsWith(link))
-            if (row) {
-                if (row[4] !== trLink) {
-                    console.log(`${link}: ${row[4]} -> ${trLink} (${trUsername})`)
-                    try {
-                        row[5] = await getAuthorUid(trUsername, trLink)
-                        row[3] = trTitle
-                        row[4] = trLink
-                        row[6] = '1'
-                        count++
-                    } catch (e) {
-                        console.error(e)
+        const html = await get(McbbsIndexUri)
+        let match = regex.exec(html)
+        while (match) {
+            const [, link, trLink, trTitle, trUsername] = match
+            if (/^https?:\/\/(?:www\.)?mcbbs\.net\/thread\-\d+\-\d+\-\d+\.html?$/.test(trLink) && trUsername !== '-') {
+                const row = rawTable.find(r => r[2] && r[2].endsWith(link))
+                if (row) {
+                    if (row[4] !== trLink) {
+                        console.log(`${link}: ${row[4]} -> ${trLink} (${trUsername})`)
+                        try {
+                            row[5] = await getAuthorUid(trUsername, trLink)
+                            row[3] = trTitle
+                            row[4] = trLink
+                            row[6] = '1'
+                            count++
+                        } catch (e) {
+                            console.error(e)
+                        }
                     }
+                } else {
+                    console.warn(`Faild to find article ‘${link}’ in the raw table.`)
                 }
-            } else {
-                console.warn(`Faild to find article ‘${link}’ in the raw table.`)
             }
+            match = regex.exec(html)
         }
-        match = regex.exec(html)
+    } catch (e) {
+        console.error(e)
     }
 }
 
@@ -116,4 +126,9 @@ async function sync() {
     }
 }
 
-sync()
+Promise
+    .race(sync(), new Promise(resolve => setTimeout(resolve, 60_000)))
+    .then(() => {
+        console.log("Process exit.")
+        process.exit()
+    })
